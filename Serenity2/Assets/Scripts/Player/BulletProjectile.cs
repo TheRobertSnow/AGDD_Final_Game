2790e5ad17771b8af67cfe8af35a5acf6f0ded3d
arnar-steinthors <arnardadi99@hotmail.com>
2022-03-19 21:11:03 +0000

    bullets show up for all players

--- Serenity2/Assets/Scripts/Player/BulletProjectile.cs
+++ Serenity2/Assets/Scripts/Player/BulletProjectile.cs
@@ -1,10 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
-public class BulletProjectile : MonoBehaviour
+public class BulletProjectile : MonoBehaviourPun, IPunInstantiateMagicCallback
 {
+    public Rigidbody rb;
+
     void OnCollisionEnter(Collision other) {
         Destroy(gameObject);
+    }
+    public void OnPhotonInstantiate(PhotonMessageInfo info)
+    {
+        object[] data = info.photonView.InstantiationData;
+        foreach (var velocity in data)
+        {
+            rb.velocity = (Vector3) velocity;
+        }
     }
 }
