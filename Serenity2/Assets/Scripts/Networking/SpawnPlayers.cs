873080ac22d52f5794973bdf950bee288b8452a9
Eiríkur Jóhannsson <eiki@live.com>
2022-04-05 21:59:54 +0000

    Respawn fixed

--- Serenity2/Assets/Scripts/Networking/SpawnPlayers.cs
+++ Serenity2/Assets/Scripts/Networking/SpawnPlayers.cs
@@ -10,25 +10,62 @@ public class SpawnPlayers : MonoBehaviour
     public Transform[] blueTeamSpawnPoints;
     public Transform[] redTeamSpawnPoints;
     public Transform noTeamSpawnPoint;
-    Transform spawnPoint;
-    int redTeamCount;
-    int blueTeamCount;
+    public Transform redTeamRespawnPoint;
+    public Transform blueTeamRespawnPoint;
+
+    GameObject _player;
+    Transform _spawnPoint;
+    int _playerTeam;
+
     void Start()
+    {
+        _playerTeam = (int)PhotonNetwork.LocalPlayer.CustomProperties["team"];
+        if (_playerTeam == 0)
+        {
+            _spawnPoint = blueTeamSpawnPoints[(int)PhotonNetwork.LocalPlayer.CustomProperties["spawnPoint"]];
+        }
+        else if (_playerTeam == 1)
+        {
+            _spawnPoint = redTeamSpawnPoints[(int)PhotonNetwork.LocalPlayer.CustomProperties["spawnPoint"]];
+        }
+        else
+        {
+            _spawnPoint = noTeamSpawnPoint;
+        }
+        _player = PhotonNetwork.Instantiate(playerPrefab.name, _spawnPoint.position, Quaternion.identity);
+    }
+
+    public void NewRound()
     {
         int playerTeam = (int)PhotonNetwork.LocalPlayer.CustomProperties["team"];
         if (playerTeam == 0)
         {
-            spawnPoint = blueTeamSpawnPoints[(int)PhotonNetwork.LocalPlayer.CustomProperties["spawnPoint"]];
+            _spawnPoint = blueTeamSpawnPoints[(int)PhotonNetwork.LocalPlayer.CustomProperties["spawnPoint"]];
         }
         else if (playerTeam == 1)
         {
-            spawnPoint = redTeamSpawnPoints[(int)PhotonNetwork.LocalPlayer.CustomProperties["spawnPoint"]];
+            _spawnPoint = redTeamSpawnPoints[(int)PhotonNetwork.LocalPlayer.CustomProperties["spawnPoint"]];
         }
         else
         {
-            spawnPoint = noTeamSpawnPoint;
+            _spawnPoint = noTeamSpawnPoint;
         }
-        PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, Quaternion.identity);
+        _player.transform.position = _spawnPoint.position;
+    }
+
+    private void Update()
+    {
+        if (_player.transform.position.y < -20)
+        {
+            if (_playerTeam == 0)
+            {
+                _player.transform.position = blueTeamRespawnPoint.position;
+            }
+            else if (_playerTeam == 1)
+            {
+                _player.transform.position = redTeamRespawnPoint.position;
+            }
+        }
     }
 }
 
