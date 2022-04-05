32ddaa3363c2359938c3410f6ccd46898a452154
arnar-steinthors <arnardadi99@hotmail.com>
2022-04-05 19:20:14 +0000

    target locations are events and target color

--- Serenity2/Assets/Scripts/Player/Eiki/Target.cs
+++ Serenity2/Assets/Scripts/Player/Eiki/Target.cs
@@ -12,13 +12,13 @@ public class Target : MonoBehaviour
 
     // if it randomizes to a moving target, then this is the speed
     public float targetSpeed = 2.5f;
-    
+
     // if it is not a moving target then the size will be scaled somewhere between the scale mix/max
     public float sizeScaleMax = 1.0f;
     public float sizeScaleMin = 0.5f;
 
 
-    private bool isMovingTarget;
+    private bool isMovingTarget = false;
     private float minY;
     private float maxY;
     private float minZ;
@@ -31,7 +31,7 @@ public class Target : MonoBehaviour
         _photonView = GetComponent<PhotonView>();
     }
 
-    private void Start() 
+    private void Start()
     {
         targetManager = GameObject.Find("TargetManager").GetComponent<TargetManager>();
         healthController = GameObject.Find("HealthController").GetComponent<HealthController>();
@@ -40,24 +40,41 @@ public class Target : MonoBehaviour
         maxY = targetManager.maxY;
         maxZ = targetManager.maxZ;
 
-        destination = GetNewDestination();
-        isMovingTarget = Random.Range(0, 4) == 1;
 
-        if (!isMovingTarget)
+        if (!PhotonNetwork.IsMasterClient) return;
+        // only the master client will set sizes/directions
+        isMovingTarget = Random.Range(0, 4) == 1;
+        if (isMovingTarget)
+        {
+            _photonView.RPC(nameof(GetNewDestination), RpcTarget.All, Random.Range(minY, maxY),
+                Random.Range(minZ, maxZ));
+        }
+        else
         {
             float scale = Random.Range(sizeScaleMin, sizeScaleMax);
-            transform.localScale = new Vector3(scale, transform.localScale.y, scale);
+            _photonView.RPC(nameof(SetScale), RpcTarget.All, scale, scale);
+            // transform.localScale = new Vector3(scale, transform.localScale.y, scale);
         }
     }
 
-    private Vector3 GetNewDestination()
+    [PunRPC]
+    private void SetScale(float x, float z)
     {
-        return new Vector3(0, Random.Range(minY, maxY), Random.Range(minZ, maxZ));
+        transform.localScale = new Vector3(x, transform.localScale.y, z);
     }
 
-    void OnCollisionEnter(Collision other) {
+    [PunRPC]
+    private void GetNewDestination(float y, float z)
+    {
+        isMovingTarget = true;
+        destination = new Vector3(0, y, z);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
         BulletProjectile bullet = other.gameObject.GetComponent<BulletProjectile>();
-        if(bullet != null) {
+        if (bullet != null)
+        {
             // if (bullet.team == 0) {
             //     healthController.SetHealthRedTeam(10);
             // }
@@ -71,9 +88,7 @@ public class Target : MonoBehaviour
                 PhotonNetwork.Destroy(_photonView.gameObject);
                 targetManager.GetComponent<TargetManager>().wasHit = true;
                 PhotonNetwork.Destroy(other.gameObject);
-                
             }
-            
         }
     }
 
@@ -98,9 +113,11 @@ public class Target : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, destination, step);
 
         // if we reach the destination we make a new one.
-        if (transform.position == destination)
+        if (transform.position == destination && PhotonNetwork.IsMasterClient)
         {
-            destination = GetNewDestination();
+            _photonView.RPC(nameof(GetNewDestination), RpcTarget.All, Random.Range(minY, maxY),
+                Random.Range(minZ, maxZ));
+            // GetNewDestination(Random.Range(minY, maxY),Random.Range(minZ, maxZ));
         }
     }
 }
